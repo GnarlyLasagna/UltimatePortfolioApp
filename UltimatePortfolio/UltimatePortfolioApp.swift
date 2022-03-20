7061893066fd81c4a620ffdc184d9aef79b28600
Evan Dolatowski <86629089+GnarlyLasagna@users.noreply.github.com>
2022-03-20 00:29:34 -0500

    Add files via upload

--- UltimatePortfolio/UltimatePortfolioApp.swift
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -13,9 +13,13 @@ struct UltimatePortfolioApp: App {
                 .environment(\.managedObjectContext, dataController.container.viewContext)
                 .environmentObject(dataController)
                 .onReceive(
-                    NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification),
-                    perform: save
-                )
+                    // Automatically save when we detect that we are
+                    // no longer the foreground app. Use this rather than
+                    // scene phase so we can port to macOS, where scene
+                    // phase won't detect our app losing focus.
+                NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification),
+                perform: save
+            )
         }
     }
     func save(_ note: Notification) {
