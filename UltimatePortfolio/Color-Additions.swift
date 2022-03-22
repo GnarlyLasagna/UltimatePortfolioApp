3fb655a172590bcd2d6d2f02ba591c2f10bf609f
Evan Dolatowski <86629089+GnarlyLasagna@users.noreply.github.com>
2022-03-22 08:36:38 -0500

    Delete Color-Additions.swift

--- UltimatePortfolio/Color-Additions.swift
+++
@@ -1,6 +0,0 @@
-import SwiftUI
-
-extension Color {
-    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
-    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
-}
